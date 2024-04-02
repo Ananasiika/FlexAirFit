e95736068f82757f340478ba26fe527cc34b15fa
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:33:22 +0300

    Update ScheduleDbModel.cs

--- FlexAirFit/FlexAirFit.Database/Models/ScheduleDbModel.cs
+++ FlexAirFit/FlexAirFit.Database/Models/ScheduleDbModel.cs
@@ -2,37 +2,36 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace FlexAirFit.Database.Models
+namespace FlexAirFit.Database.Models;
+
+public class ScheduleDbModel
 {
-    public class ScheduleDbModel
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [Column("workout_id")]
+    [ForeignKey("Workout")]
+    public Guid IdWorkout { get; set; }
+
+    [Required]
+    [Column("date_and_time")]
+    public DateTime DateAndTime { get; set; }
+
+    [Column("client_id")]
+    [ForeignKey("Client")]
+    public Guid? IdClient { get; set; }
+
+    public ScheduleDbModel(Guid id, Guid idWorkout, DateTime dateAndTime, Guid? idClient)
     {
-        [Key]
-        [Column("id")]
-        public Guid Id { get; set; }
-
-        [Required]
-        [Column("workout_id")]
-        [ForeignKey("Workout")]
-        public Guid IdWorkout { get; set; }
-
-        [Required]
-        [Column("date_and_time")]
-        public DateTime DateAndTime { get; set; }
-
-        [Column("client_id")]
-        [ForeignKey("Client")]
-        public Guid? IdClient { get; set; }
-
-        public ScheduleDbModel(Guid id, Guid idWorkout, DateTime dateAndTime, Guid? idClient)
-        {
-            Id = id;
-            IdWorkout = idWorkout;
-            DateAndTime = dateAndTime;
-            IdClient = idClient;
-        }
-
-        public virtual WorkoutDbModel Workout { get; set; }
-
-        public virtual ClientDbModel Client { get; set; }
+        Id = id;
+        IdWorkout = idWorkout;
+        DateAndTime = dateAndTime;
+        IdClient = idClient;
     }
+
+    public virtual WorkoutDbModel Workout { get; set; }
+
+    public virtual ClientDbModel Client { get; set; }
 }
