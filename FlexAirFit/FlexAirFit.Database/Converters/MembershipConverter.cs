2a82857842e84f1e3b0e0586854779136d8cd76c
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:30:52 +0300

    Update MembershipConverter.cs

--- FlexAirFit/FlexAirFit.Database/Converters/MembershipConverter.cs
+++ FlexAirFit/FlexAirFit.Database/Converters/MembershipConverter.cs
@@ -2,34 +2,33 @@
 using FlexAirFit.Core.Models;
 using FlexAirFit.Database.Models;
 
-namespace FlexAirFit.Database.Models.Converters
-{
-    public static class MembershipConverter
-    {
-        [return: NotNullIfNotNull(nameof(model))]
-        public static Membership? DbToCoreModel(MembershipDbModel? model)
-        {
-            return model is not null
-                ? new Membership(
-                    id: model.Id,
-                    name: model.Name,
-                    duration: model.Duration,
-                    price: model.Price,
-                    freezing: model.Freezing)
-                : default;
-        }
+namespace FlexAirFit.Database.Models.Converters;
 
-        [return: NotNullIfNotNull(nameof(model))]
-        public static MembershipDbModel? CoreToDbModel(Membership? model)
-        {
-            return model is not null
-                ? new MembershipDbModel(
-                    id: model.Id,
-                    name: model.Name,
-                    duration: model.Duration,
-                    price: model.Price,
-                    freezing: model.Freezing)
-                : default;
-        }
+public static class MembershipConverter
+{
+    [return: NotNullIfNotNull(nameof(model))]
+    public static Membership? DbToCoreModel(MembershipDbModel? model)
+    {
+        return model is not null
+            ? new Membership(
+                id: model.Id,
+                name: model.Name,
+                duration: model.Duration,
+                price: model.Price,
+                freezing: model.Freezing)
+            : default;
+    }
+
+    [return: NotNullIfNotNull(nameof(model))]
+    public static MembershipDbModel? CoreToDbModel(Membership? model)
+    {
+        return model is not null
+            ? new MembershipDbModel(
+                id: model.Id,
+                name: model.Name,
+                duration: model.Duration,
+                price: model.Price,
+                freezing: model.Freezing)
+            : default;
     }
 }
