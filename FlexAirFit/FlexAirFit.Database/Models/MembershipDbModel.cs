09da272de870cf723f7d1c53c006dcffb8b2a7ac
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:33:01 +0300

    Update MembershipDbModel.cs

--- FlexAirFit/FlexAirFit.Database/Models/MembershipDbModel.cs
+++ FlexAirFit/FlexAirFit.Database/Models/MembershipDbModel.cs
@@ -2,37 +2,36 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace FlexAirFit.Database.Models
+namespace FlexAirFit.Database.Models;
+
+public class MembershipDbModel
 {
-    public class MembershipDbModel
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [Column("name", TypeName = "varchar(50)")]
+    public string Name { get; set; }
+
+    [Required]
+    [Column("duration", TypeName = "time")]
+    public TimeSpan Duration { get; set; }
+
+    [Required]
+    [Column("price")]
+    public int Price { get; set; }
+
+    [Required]
+    [Column("freezing")]
+    public int Freezing { get; set; }
+
+    public MembershipDbModel(Guid id, string name, TimeSpan duration, int price, int freezing)
     {
-        [Key]
-        [Column("id")]
-        public Guid Id { get; set; }
-
-        [Required]
-        [Column("name", TypeName = "varchar(50)")]
-        public string Name { get; set; }
-
-        [Required]
-        [Column("duration", TypeName = "time")]
-        public TimeSpan Duration { get; set; }
-
-        [Required]
-        [Column("price")]
-        public int Price { get; set; }
-
-        [Required]
-        [Column("freezing")]
-        public int Freezing { get; set; }
-
-        public MembershipDbModel(Guid id, string name, TimeSpan duration, int price, int freezing)
-        {
-            Id = id;
-            Name = name;
-            Duration = duration;
-            Price = price;
-            Freezing = freezing;
-        }
+        Id = id;
+        Name = name;
+        Duration = duration;
+        Price = price;
+        Freezing = freezing;
     }
 }
