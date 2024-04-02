e089aef1c9a3fcd5b5d41f21f8f760039296a6e2
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:30:34 +0300

    Update ClientConverter.cs

--- FlexAirFit/FlexAirFit.Database/Converters/ClientConverter.cs
+++ FlexAirFit/FlexAirFit.Database/Converters/ClientConverter.cs
@@ -3,44 +3,43 @@ using System.Linq;
 using FlexAirFit.Core.Models;
 using FlexAirFit.Database.Models;
 
-namespace FlexAirFit.Database.Models.Converters
-{
-    public static class ClientConverter
-    {
-        [return: NotNullIfNotNull(nameof(model))]
-        public static Client? DbToCoreModel(ClientDbModel? model)
-        {
-            return model is not null
-                ? new Client(
-                    id: model.Id,
-                    idUser: model.IdUser,
-                    name: model.Name,
-                    gender: model.Gender,
-                    dateOfBirth: model.DateOfBirth,
-                    idMembership: model.IdMembership,
-                    membershipEnd: model.MembershipEnd,
-                    remainFreezing: model.RemainFreezing,
-                    isFreezing: model.IsFreezing,
-                    freezingIntervals: model.FreezingIntervals?.Select(fi => new Tuple<DateOnly, DateOnly>(fi.Item1, fi.Item2)).ToList())
-                : default;
-        }
+namespace FlexAirFit.Database.Models.Converters;
 
-        [return: NotNullIfNotNull(nameof(model))]
-        public static ClientDbModel? CoreToDbModel(Client? model)
-        {
-            return model is not null
-                ? new ClientDbModel(
-                    id: model.Id,
-                    idUser: model.IdUser,
-                    name: model.Name,
-                    gender: model.Gender,
-                    dateOfBirth: model.DateOfBirth,
-                    idMembership: model.IdMembership,
-                    membershipEnd: model.MembershipEnd,
-                    remainFreezing: model.RemainFreezing,
-                    isFreezing: model.IsFreezing,
-                    freezingIntervals: model.FreezingIntervals?.Select(fi => new Tuple<DateOnly, DateOnly>(fi.Item1, fi.Item2)).ToList())
-                : default;
-        }
+public static class ClientConverter
+{
+    [return: NotNullIfNotNull(nameof(model))]
+    public static Client? DbToCoreModel(ClientDbModel? model)
+    {
+        return model is not null
+            ? new Client(
+                id: model.Id,
+                idUser: model.IdUser,
+                name: model.Name,
+                gender: model.Gender,
+                dateOfBirth: model.DateOfBirth,
+                idMembership: model.IdMembership,
+                membershipEnd: model.MembershipEnd,
+                remainFreezing: model.RemainFreezing,
+                isFreezing: model.IsFreezing,
+                freezingIntervals: model.FreezingIntervals?.Select(fi => new Tuple<DateOnly, DateOnly>(fi.Item1, fi.Item2)).ToList())
+            : default;
+    }
+
+    [return: NotNullIfNotNull(nameof(model))]
+    public static ClientDbModel? CoreToDbModel(Client? model)
+    {
+        return model is not null
+            ? new ClientDbModel(
+                id: model.Id,
+                idUser: model.IdUser,
+                name: model.Name,
+                gender: model.Gender,
+                dateOfBirth: model.DateOfBirth,
+                idMembership: model.IdMembership,
+                membershipEnd: model.MembershipEnd,
+                remainFreezing: model.RemainFreezing,
+                isFreezing: model.IsFreezing,
+                freezingIntervals: model.FreezingIntervals?.Select(fi => new Tuple<DateOnly, DateOnly>(fi.Item1, fi.Item2)).ToList())
+            : default;
     }
 }
