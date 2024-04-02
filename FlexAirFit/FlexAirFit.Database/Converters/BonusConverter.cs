e98bca5f256446b0211233a313b592fcb7e8336c
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:30:14 +0300

    Update BonusConverter.cs

--- FlexAirFit/FlexAirFit.Database/Converters/BonusConverter.cs
+++ FlexAirFit/FlexAirFit.Database/Converters/BonusConverter.cs
@@ -2,30 +2,29 @@
 using FlexAirFit.Core.Models;
 using FlexAirFit.Database.Models;
 
-namespace FlexAirFit.Database.Models.Converters
-{
-    public static class BonusConverter
-    {
-        [return: NotNullIfNotNull(nameof(model))]
-        public static Bonus? DbToCoreModel(BonusDbModel? model)
-        {
-            return model is not null
-                ? new Bonus(
-                    id: model.Id,
-                    idClient: model.IdClient,
-                    count: model.Count)
-                : default;
-        }
+namespace FlexAirFit.Database.Models.Converters;
 
-        [return: NotNullIfNotNull(nameof(model))]
-        public static BonusDbModel? CoreToDbModel(Bonus? model)
-        {
-            return model is not null
-                ? new BonusDbModel(
-                    id: model.Id,
-                    idClient: model.IdClient,
-                    count: model.Count)
-                : default;
-        }
+public static class BonusConverter
+{
+    [return: NotNullIfNotNull(nameof(model))]
+    public static Bonus? DbToCoreModel(BonusDbModel? model)
+    {
+        return model is not null
+            ? new Bonus(
+                id: model.Id,
+                idClient: model.IdClient,
+                count: model.Count)
+            : default;
+    }
+
+    [return: NotNullIfNotNull(nameof(model))]
+    public static BonusDbModel? CoreToDbModel(Bonus? model)
+    {
+        return model is not null
+            ? new BonusDbModel(
+                id: model.Id,
+                idClient: model.IdClient,
+                count: model.Count)
+            : default;
     }
 }
