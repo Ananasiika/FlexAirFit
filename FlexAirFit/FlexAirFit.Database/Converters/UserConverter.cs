17b6f0183b8f669756fa77a830d9b29b1b457cc1
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:31:59 +0300

    Update UserConverter.cs

--- FlexAirFit/FlexAirFit.Database/Converters/UserConverter.cs
+++ FlexAirFit/FlexAirFit.Database/Converters/UserConverter.cs
@@ -3,32 +3,31 @@ using FlexAirFit.Core.Enums;
 using FlexAirFit.Core.Models;
 using FlexAirFit.Database.Models;
 
-namespace FlexAirFit.Database.Models.Converters
-{
-    public static class UserConverter
-    {
-        [return: NotNullIfNotNull(nameof(model))]
-        public static User? DbToCoreModel(UserDbModel? model)
-        {
-            return model is not null
-                ? new User(
-                    id: model.Id,
-                    role: model.Role,
-                    email: model.Email,
-                    passwordHashed: model.PasswordHashed)
-                : default;
-        }
+namespace FlexAirFit.Database.Models.Converters;
 
-        [return: NotNullIfNotNull(nameof(model))]
-        public static UserDbModel? CoreToDbModel(User? model)
-        {
-            return model is not null
-                ? new UserDbModel(
-                    id: model.Id,
-                    role: model.Role,
-                    email: model.Email,
-                    passwordHashed: model.PasswordHashed)
-                : default;
-        }
+public static class UserConverter
+{
+    [return: NotNullIfNotNull(nameof(model))]
+    public static User? DbToCoreModel(UserDbModel? model)
+    {
+        return model is not null
+            ? new User(
+                id: model.Id,
+                role: model.Role,
+                email: model.Email,
+                passwordHashed: model.PasswordHashed)
+            : default;
+    }
+
+    [return: NotNullIfNotNull(nameof(model))]
+    public static UserDbModel? CoreToDbModel(User? model)
+    {
+        return model is not null
+            ? new UserDbModel(
+                id: model.Id,
+                role: model.Role,
+                email: model.Email,
+                passwordHashed: model.PasswordHashed)
+            : default;
     }
 }
