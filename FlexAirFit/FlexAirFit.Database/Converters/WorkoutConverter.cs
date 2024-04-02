7a10d11485a36db4bce6ded413d687a13ab95cf8
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:32:14 +0300

    Update WorkoutConverter.cs

--- FlexAirFit/FlexAirFit.Database/Converters/WorkoutConverter.cs
+++ FlexAirFit/FlexAirFit.Database/Converters/WorkoutConverter.cs
@@ -2,36 +2,35 @@
 using FlexAirFit.Core.Models;
 using FlexAirFit.Database.Models;
 
-namespace FlexAirFit.Database.Models.Converters
-{
-    public static class WorkoutConverter
-    {
-        [return: NotNullIfNotNull(nameof(model))]
-        public static Workout? DbToCoreModel(WorkoutDbModel? model)
-        {
-            return model is not null
-                ? new Workout(
-                    id: model.Id,
-                    name: model.Name,
-                    description: model.Description,
-                    idTrainer: model.IdTrainer,
-                    duration: model.Duration,
-                    level: model.Level)
-                : default;
-        }
+namespace FlexAirFit.Database.Models.Converters;
 
-        [return: NotNullIfNotNull(nameof(model))]
-        public static WorkoutDbModel? CoreToDbModel(Workout? model)
-        {
-            return model is not null
-                ? new WorkoutDbModel(
-                    id: model.Id,
-                    name: model.Name,
-                    description: model.Description,
-                    idTrainer: model.IdTrainer,
-                    duration: model.Duration,
-                    level: model.Level)
-                : default;
-        }
+public static class WorkoutConverter
+{
+    [return: NotNullIfNotNull(nameof(model))]
+    public static Workout? DbToCoreModel(WorkoutDbModel? model)
+    {
+        return model is not null
+            ? new Workout(
+                id: model.Id,
+                name: model.Name,
+                description: model.Description,
+                idTrainer: model.IdTrainer,
+                duration: model.Duration,
+                level: model.Level)
+            : default;
+    }
+
+    [return: NotNullIfNotNull(nameof(model))]
+    public static WorkoutDbModel? CoreToDbModel(Workout? model)
+    {
+        return model is not null
+            ? new WorkoutDbModel(
+                id: model.Id,
+                name: model.Name,
+                description: model.Description,
+                idTrainer: model.IdTrainer,
+                duration: model.Duration,
+                level: model.Level)
+            : default;
     }
 }
