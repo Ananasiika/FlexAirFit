346c39e73f940c997ec22a90f588b840b9cd4ce2
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-02 16:33:51 +0300

    Update UserDbModel.cs

--- FlexAirFit/FlexAirFit.Database/Models/UserDbModel.cs
+++ FlexAirFit/FlexAirFit.Database/Models/UserDbModel.cs
@@ -3,32 +3,31 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using FlexAirFit.Core.Enums;
 
-namespace FlexAirFit.Database.Models
+namespace FlexAirFit.Database.Models;
+
+public class UserDbModel
 {
-    public class UserDbModel
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [Column("role")]
+    public UserRole Role { get; set; }
+
+    [Required]
+    [Column("email", TypeName = "varchar(100)")]
+    public string Email { get; set; }
+
+    [Required]
+    [Column("password_hashed", TypeName = "varchar(100)")]
+    public string PasswordHashed { get; set; }
+
+    public UserDbModel(Guid id, UserRole role, string email, string passwordHashed)
     {
-        [Key]
-        [Column("id")]
-        public Guid Id { get; set; }
-
-        [Required]
-        [Column("role")]
-        public UserRole Role { get; set; }
-
-        [Required]
-        [Column("email", TypeName = "varchar(100)")]
-        public string Email { get; set; }
-
-        [Required]
-        [Column("password_hashed", TypeName = "varchar(100)")]
-        public string PasswordHashed { get; set; }
-
-        public UserDbModel(Guid id, UserRole role, string email, string passwordHashed)
-        {
-            Id = id;
-            Role = role;
-            Email = email;
-            PasswordHashed = passwordHashed;
-        }
+        Id = id;
+        Role = role;
+        Email = email;
+        PasswordHashed = passwordHashed;
     }
 }
