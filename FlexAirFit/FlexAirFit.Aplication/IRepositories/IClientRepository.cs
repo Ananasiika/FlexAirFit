1cd3e04deb718a9e8c4b0040fd5a69fe4ed7084f
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-09 16:34:55 +0300

    Update IClientRepository.cs

--- FlexAirFit/FlexAirFit.Aplication/IRepositories/IClientRepository.cs
+++ FlexAirFit/FlexAirFit.Aplication/IRepositories/IClientRepository.cs
@@ -5,7 +5,7 @@ namespace FlexAirFit.Application.IRepositories;
 public interface IClientRepository
 {
     Task AddClientAsync(Client client);
-    Task<Client> UpdateClientAsync(Client client);
+    Task<Client> UpdateClientAsync(Guid idClient, string? name, string? gender, DateOnly? dateOfBirth, Guid? idMembership, DateOnly? membershipEnd, int? remainFreezing, List<Tuple<DateOnly, DateOnly>>? freezingIntervals);
     Task DeleteClientAsync(Guid id);
     Task<Client> GetClientByIdAsync(Guid id);
     Task<List<Client>> GetClientsAsync(int? limit, int? offset);
