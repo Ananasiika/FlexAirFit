ed6fd6a20e9d7de7d745dad31a5ecad25a3a4e4a
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-09 16:32:17 +0300

    Update IClientService.cs

--- FlexAirFit/FlexAirFit.Aplication/IServices/IClientService.cs
+++ FlexAirFit/FlexAirFit.Aplication/IServices/IClientService.cs
@@ -5,7 +5,7 @@ namespace FlexAirFit.Application.IServices;
 public interface IClientService
 {
     Task CreateClient(Client client);
-    Task<Client> UpdateClient(Client client);
+    Task<Client> UpdateClient(Guid idClient, string? name, string? gender, DateOnly? DateOfBirth, Guid? IdMembership, DateOnly? MembershipEnd, int? RemainFreezing, List<Tuple<DateOnly, DateOnly>>? FreezingIntervals);
     Task DeleteClient(Guid id);
     Task<Client> GetClientById(Guid id);
     Task<List<Client>> GetClients(int? limit, int? offset);
