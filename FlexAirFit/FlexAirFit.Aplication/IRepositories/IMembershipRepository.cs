1642713bbba29040aaea7352b109c2773a500c7d
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-09 16:18:52 +0300

    Update IMembershipRepository.cs

--- FlexAirFit/FlexAirFit.Aplication/IRepositories/IMembershipRepository.cs
+++ FlexAirFit/FlexAirFit.Aplication/IRepositories/IMembershipRepository.cs
@@ -5,7 +5,7 @@ namespace FlexAirFit.Application.IRepositories;
 public interface IMembershipRepository
 {
     Task AddMembershipAsync(Membership membership);
-    Task<Membership> UpdateMembershipAsync(Membership membership);
+    Task<Membership> UpdateMembershipAsync(Guid membershipId, string? name, TimeSpan? duration, int? price, int? freezing);
     Task DeleteMembershipAsync(Guid id);
     Task<Membership> GetMembershipByIdAsync(Guid id);
     Task<List<Membership>> GetMembershipsAsync(int? limit, int? offset);
