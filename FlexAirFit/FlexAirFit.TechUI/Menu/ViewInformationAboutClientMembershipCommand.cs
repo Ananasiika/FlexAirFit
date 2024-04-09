d68a30f7852d4b9385e8bc53b57595ccf24d465d
Pronina Larisa <93927009+Ananasiika@users.noreply.github.com>
2024-04-09 16:28:16 +0300

    Create ViewInformationAboutClientMembershipCommand.cs

---
+++ FlexAirFit/FlexAirFit.TechUI/Menu/ViewInformationAboutClientMembershipCommand.cs
@@ -0,0 +1,17 @@
+﻿using FlexAirFit.TechUI.BaseMenu;
+
+namespace FlexAirFit.TechUI.Commands.ClientCommands;
+
+public class ViewInformationAboutClientMembershipCommand : Command
+{
+    public override string? Description()
+    {
+        return "Просмотр информации об абонементе";
+    }
+
+    public override async Task Execute(Context context)
+    {
+        Console.WriteLine($"Дата окончания абонемента: {context.CurrentUser.MembershipEnd}");
+        Console.WriteLine($"Количество оставшихся дней для заморозки: {context.CurrentUser.FreezingRemain}");
+    }
+}
